e1214094433bc3499a8061c2f5b5d947ac3381d5
MrJambix <131601090+MrJambix@users.noreply.github.com>
2023-09-10 01:35:01 +0200

    Update NodeTracker.cs

--- C#-Scripts-UnityEngine/NodeTracker.cs
+++ C#-Scripts-UnityEngine/NodeTracker.cs
@@ -1,129 +1,178 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 using System.Windows.Forms;
 
-namespace ResourceTrackerLibrary
+namespace NodeTracker
 {
-	// Token: 0x020007F9 RID: 2041
-	public class ResourceTracker
-	{
-		// Token: 0x0600346E RID: 13422
-		public ResourceTracker()
-		{
-			this.nodes = new List<ResourceNode__5>();
-			this.player = new Player__5(0f, 0f, 0f);
-			this.ShowGUI();
-		}
+    public class Player
+    {
+        public float X, Y, Z;
 
-		// Token: 0x0600346F RID: 13423
-		public void SetPlayerPosition(float x, float y, float z)
-		{
-			this.player = new Player__5(x, y, z);
-		}
+        public Player(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
 
-		// Token: 0x06003470 RID: 13424
-		public float GetDistanceToNearestNode()
-		{
-			if (this.nodes.Count == 0)
-			{
-				throw new Exception("No resource nodes have been added.");
-			}
-			float shortestDistance = float.MaxValue;
-			foreach (ResourceNode__5 node in from n in this.nodes
-			where n.Type == this.selectedResource
-			select n)
-			{
-				float distance = this.CalculateDistance(this.player, node);
-				if (distance < shortestDistance)
-				{
-					shortestDistance = distance;
-				}
-			}
-			return shortestDistance;
-		}
+    public class ResourceNode
+    {
+        public float X, Y, Z;
+        public string Type;  // e.g., "Copper Vein", "Iron Vein", "Coal", etc.
 
-		// Token: 0x060034C2 RID: 13506
-		public void AddNode(ResourceNode__5 node)
-		{
-			this.nodes.Add(node);
-		}
+        public ResourceNode(float x, float y, float z, string type)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Type = type;
+        }
+    }
 
-		// Token: 0x060034C5 RID: 13509
-		private float CalculateDistance(Player__5 player, ResourceNode__5 node)
-		{
-			float num = player.X - node.X;
-			float dy = player.Y - node.Y;
-			float dz = player.Z - node.Z;
-			return (float)Math.Sqrt((double)(num * num + dy * dy + dz * dz));
-		}
+    public class ResourceTracker : Form
+    {
+        private Player player;
+        private List<ResourceNode> nodes;
 
-		// Token: 0x060034C6 RID: 13510
-		private void ShowGUI()
-		{
-			Form form = new Form
-			{
-				Text = "Resource Tracker",
-				Size = new Size(300, 150)
-			};
-			Label label = new Label
-			{
-				Text = "Select Resource Type",
-				Location = new Point(10, 10),
-				AutoSize = true
-			};
-			form.Controls.Add(label);
-			ComboBox resourceOptions = new ComboBox
-			{
-				Location = new Point(10, 30),
-				DropDownStyle = ComboBoxStyle.DropDownList
-			};
-			string[] minerals = new string[]
-			{
-				"Coal",
-				"Copper Vein Small",
-				"Copper Vein Large",
-				"Iron Vein Small",
-				"Iron Vein Large",
-				"Gold Small",
-				"Gold Large",
-				"Silver Small",
-				"Silver Large",
-				"Platinum Small",
-				"Platinum Large",
-				"Palladium Small",
-				"Palladium Large",
-				"Feygold Small",
-				"Feygold Large",
-				"Crimsonite Small",
-				"Crimsonite Large",
-				"Celestium Small",
-				"Celestium Large",
-				"Azurium Small",
-				"Azurium Large",
-				"Mystril Small",
-				"Mystril Large"
-			};
-			ComboBox.ObjectCollection items3 = resourceOptions.Items;
-			object[] array = minerals;
-			object[] items2 = array;
-			items3.AddRange(items2);
-			form.Controls.Add(resourceOptions);
-			resourceOptions.SelectedIndexChanged += delegate(object sender, EventArgs e)
-			{
-				this.selectedResource = resourceOptions.SelectedItem.ToString();
-			};
-			Application.Run(form);
-		}
+        // GUI Components
+        private ComboBox mineralComboBox, herbalismComboBox, woodCuttingComboBox, sizeComboBox;
+        private CheckBox trackMineralCheckBox, trackHerbCheckBox, trackTreeCheckBox, showArrowCheckBox;
+        private Label distanceLabel;
+        private Button startButton;
 
-		// Token: 0x04002D82 RID: 11650
-		private List<ResourceNode__5> nodes;
+        public ResourceTracker()
+        {
+            this.nodes = new List<ResourceNode>();
+            this.player = new Player(0, 0, 0);
 
-		// Token: 0x04002D83 RID: 11651
-		private Player__5 player;
+            // GUI Initialization
+            InitializeComponents();
 
-		// Token: 0x04002D84 RID: 11652
-		private string selectedResource = "";
-	}
+            // This timer will keep updating the GUI
+            Timer updateTimer = new Timer();
+            updateTimer.Interval = 1000;  // 1 second
+            updateTimer.Tick += (s, e) => Update();
+            updateTimer.Start();
+        }
+
+        private void InitializeComponents()
+        {
+            this.distanceLabel = new Label() { Location = new Point(10, 10), Width = 200 };
+            this.Controls.Add(distanceLabel);
+
+            this.mineralComboBox = new ComboBox() { Location = new Point(10, 40), Width = 100 };
+            this.mineralComboBox.Items.AddRange(new string[] {
+                "Copper", "Iron", "Coal", "Gold", "Silver",
+                "Platinum", "Palladium", "Feygold", "Crimsonite",
+                "Celestium", "Azurium", "Mystril"
+            });
+            this.Controls.Add(mineralComboBox);
+
+            this.herbalismComboBox = new ComboBox() { Location = new Point(120, 40), Width = 100 };
+            this.herbalismComboBox.Items.AddRange(new string[] {
+                "Hemp", "Rinthistle", "Redban", "Sunthistle", "FrostFlower",
+                "Nepbloom", "Minweed", "Oxbloom", "Ginshade", "Aetherthistle",
+                "Starstem", "Flax", "Wistera", "Cotton", "Arcanebloom",
+                "Elvenbloom", "Potato"
+            });
+            this.Controls.Add(herbalismComboBox);
+
+            this.woodCuttingComboBox = new ComboBox() { Location = new Point(230, 40), Width = 100 };
+            this.woodCuttingComboBox.Items.AddRange(new string[] {
+                "Acacia Tree", "Wispwood Tree", "Staroak Tree", "Aetherbark Tree"
+            });
+            this.Controls.Add(woodCuttingComboBox);
+
+            this.trackMineralCheckBox = new CheckBox() { Location = new Point(10, 70), Text = "Track Minerals" };
+            this.Controls.Add(trackMineralCheckBox);
+
+            this.trackHerbCheckBox = new CheckBox() { Location = new Point(120, 70), Text = "Track Herbs" };
+            this.Controls.Add(trackHerbCheckBox);
+
+            this.trackTreeCheckBox = new CheckBox() { Location = new Point(230, 70), Text = "Track Trees" };
+            this.Controls.Add(trackTreeCheckBox);
+
+            this.showArrowCheckBox = new CheckBox() { Location = new Point(10, 100), Text = "Show Arrow" };
+            this.Controls.Add(showArrowCheckBox);
+
+            this.sizeComboBox = new ComboBox() { Location = new Point(10, 130), Width = 100 };
+            this.sizeComboBox.Items.AddRange(new string[] { "Small", "Medium", "Large" });
+            this.sizeComboBox.SelectedIndexChanged += (s, e) => ResizeForm(sizeComboBox.SelectedItem.ToString());
+            this.Controls.Add(sizeComboBox);
+
+            this.startButton = new Button() { Location = new Point(10, 160), Text = "Start" };
+            this.startButton.Click += (s, e) => Update();
+            this.Controls.Add(startButton);
+
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.Size = new Size(350, 250);
+        }
+
+        private void ResizeForm(string size)
+        {
+            switch (size)
+            {
+                case "Small":
+                    this.Size = new Size(200, 150);
+                    break;
+                case "Medium":
+                    this.Size = new Size(350, 250);
+                    break;
+                case "Large":
+                    this.Size = new Size(500, 350);
+                    break;
+            }
+        }
+
+        private ResourceNode GetClosestResourceNode()
+        {
+            // Implement the logic to find the closest resource node based on the selected options
+            ResourceNode closestNode = null;
+            double minDistance = double.MaxValue;
+
+            foreach (ResourceNode node in nodes)
+            {
+                if (IsResourceTypeSelected(node.Type))
+                {
+                    double distance = GetDistance(player, node);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestNode = node;
+                    }
+                }
+            }
+
+            return closestNode;
+        }
+
+        private bool IsResourceTypeSelected(string nodeType)
+        {
+            string selectedMineral = mineralComboBox.SelectedItem.ToString();
+            if ((trackMineralCheckBox.Checked && nodeType.Contains(selectedMineral)) ||
+                (trackHerbCheckBox.Checked && herbalismComboBox.SelectedItem.ToString() == nodeType) ||
+                (trackTreeCheckBox.Checked && woodCuttingComboBox.SelectedItem.ToString() == nodeType))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private double GetDistance(Player p, ResourceNode r)
+        {
+            double dx = p.X - r.X;
+            double dy = p.Y - r.Y;
+            double dz = p.Z - r.Z;
+
+            return Math.Sqrt(dx * dx + dy * dy + dz * dz);
+        }
+
+        public static void Main()
+        {
+            Application.Run(new ResourceTracker());
+        }
+    }
 }
